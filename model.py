5bf802d24ba6481638d11c92d792f4a4fd8bd030
Karan Jindal <karanjindal.dev@gmail.com>
2026-03-27 20:06:54 +0530

    Refactor data pipeline to use tf.data API

--- model.py
+++ model.py
@@ -9,8 +9,8 @@ def unet(input_shape, filters, is_inside_notebook):
     skip3, enc3 = encoder_block(enc2, filters*4)
     skip4, enc4 = encoder_block(enc3, filters*8)
 
-    enc_pen_ultimate = tf.keras.layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='same', activation='relu')(enc4)
-    enc_ultimate = tf.keras.layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='same', activation='relu')(enc_pen_ultimate)
+    enc_pen_ultimate = tf.keras.layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer='he')(enc4)
+    enc_ultimate = tf.keras.layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='same', activation='relu',kernel_initializer='he')(enc_pen_ultimate)
 
     dec1 = decoder_block(enc_ultimate, skip4, filters*8)
     dec2 = decoder_block(dec1, skip3, filters*4)
@@ -28,16 +28,16 @@ def unet(input_shape, filters, is_inside_notebook):
     return model
 
 def encoder_block(input_, filters):
-    out1 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', activation='relu')(input_)
-    out2 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', activation='relu')(out1)
+    out1 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer='he')(input_)
+    out2 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', activation='relu', kernel_initializer='he')(out1)
     output = tf.keras.layers.MaxPool2D(strides=(2, 2))(out2)
     return out2, output
 
 def decoder_block(input_, skip, filters):
-    out1 = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=(2, 2), strides=(2, 2), padding='same', activation='relu')(input_)
+    out1 = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=(2, 2), strides=(2, 2), padding='same', activation='relu', kernel_initializer='he')(input_)
     out2 = tf.keras.layers.concatenate([skip, out1], axis=-1)
-    out3 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), activation='relu', padding='same')(out2)
-    output = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), activation='relu', padding='same')(out3)
+    out3 = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he')(out2)
+    output = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he')(out3)
     return output
 
 if __name__ == "__main__":
